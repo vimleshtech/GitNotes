a0f272bed6e705b7c0aa59312f15e99d2da6e888
saurabh <gl_saurabh@yahoo.com>
2018-02-15 10:21:53 +0530

    java release

---
+++ src/compute/startup.java
@@ -0,0 +1,18 @@
+package compute;
+
+public class startup {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		a =333;
+		b =44;
+		c = a+b;
+		
+		System.out.println(c);
+
+	}
+
+}
